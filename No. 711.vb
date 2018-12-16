6616be882b871046eb573d7fadf1ef83fab26ec5
Robert <robert1stepanyan@gmail.com>
2018-12-16 21:17:13 +0400

    Create No. 711.vb

--- No. 711.vb
+++ No. 711.vb
@@ -8,14 +8,15 @@ End If
 z = Split(x, " ")
 For i = 0 To UBound(z)
 If z(i) <> "" Then
-Text2.Text = Text2.Text & Mid(z(i), 1, 1)
+p = p & Mid(z(i), 1, 1)
 For j = 2 To Len(z(i))
 If Mid(z(i), 1, 1) = Mid(z(i), j, 1) Then
-Text2.Text = Text2.Text & "."
+p = p & "."
 Else
-Text2.Text = Text2.Text & Mid(z(i), j, 1)
+p = p & Mid(z(i), j, 1)
 End If
 Next
 End If
-Text2.Text = Text2.Text & " "
+p = p & " "
 Next
+Text1.Text = p
