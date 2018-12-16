d67d8bbd81faaf814ef717eae010e7dc2ecba4f8
Robert <robert1stepanyan@gmail.com>
2018-12-16 22:46:47 +0400

    Rename No. 720.vb to No. 722.vb

---
+++ No. 722.vb
@@ -0,0 +1,15 @@
+'No. 720
+
+x = Text1.Text
+If x = "" Then
+MsgBox "Datark e"
+Exit Sub
+End If
+j = Len(x) \ 2 + 1
+For i = 1 To Len(x) \ 2
+Text2.Text = Mid(x, j, 1) & Mid(x, i, 1) & Text2.Text
+j = j + 1
+Next
+If j <> Len(x) + 1 Then
+Text2.Text = Text2.Text & Mid(x, Len(x), 1)
+End If
