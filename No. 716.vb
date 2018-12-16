25e4f10c42dc795617d2129bc847243a8d5fcea6
Robert <robert1stepanyan@gmail.com>
2018-12-16 21:56:22 +0400

    Create No. 716.vb

---
+++ No. 716.vb
@@ -0,0 +1,27 @@
+No. 716.vb
+
+Dim k() As String
+x = Text1.Text
+If x = "" Then
+MsgBox "Datark e"
+Exit Sub
+End If
+z = Split(Text1.Text, " ")
+ReDim k(1 To UBound(z) + 1)
+For i = 0 To UBound(z)
+If z(i) <> "" Then
+j = j + 1
+k(j) = z(i)
+q = q + 1
+List2.AddItem k(j)
+End If
+Next
+Min = Len(k(1))
+Minb = k(1)
+For i = 2 To q
+If Len(k(i)) < Min Then
+Min = Len(k(i))
+Minb = k(i)
+End If
+Next
+MsgBox "Shortest: " & Minb
