c919819a6ff1785a0dac528c24bdc86d706fc006
Robert <robert1stepanyan@gmail.com>
2019-01-15 15:06:22 +0400

    Create No 38.vb

---
+++ No 38.vb
@@ -0,0 +1,9 @@
+a = Val(InputBox("A:  "))
+b = Val(InputBox("B: "))
+c = Val(InputBox("C: "))
+d = Val(InputBox("D: "))
+If d / c = c / b And c / b = b / a Then
+lst.AddItem True
+Else
+lst.AddItem False
+End If
