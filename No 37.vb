5f60cb595588448f4aa3593217f1a46b1ef67337
Robert <robert1stepanyan@gmail.com>
2019-01-15 15:03:33 +0400

    Delete No 37.vb

--- No 37.vb
+++
@@ -1,9 +0,0 @@
-a = Val(InputBox("A:  "))
-b = Val(InputBox("B: "))
-c = Val(InputBox("C: "))
-d = Val(InputBox("D: "))
-If d - c = c - b = b - a Then
-lst.AddItem True
-Else
-lst.AddItem False
-end if
