62d8ef113e3114f19def86e25877332f27a612e6
Robert <robert1stepanyan@gmail.com>
2019-01-15 14:53:16 +0400

    Create No 34.vb

---
+++ No 34.vb
@@ -0,0 +1,9 @@
+a = Val(InputBox("A:  "))
+b = Val(InputBox("B: "))
+c = Val(InputBox("C: "))
+d = Val(InputBox("D: "))
+If (a + b = c + d) Or (a + d = b + c) Then
+lst.AddItem True
+Else
+lst.AddItem False
+end if
