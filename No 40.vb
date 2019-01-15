4c5b360ba9a49b8108715611ff976c9c3a3828a3
Robert <robert1stepanyan@gmail.com>
2019-01-15 15:12:36 +0400

    Create No 40.vb

---
+++ No 40.vb
@@ -0,0 +1,18 @@
+a = Val(InputBox("A:  "))
+b = Val(InputBox("B: "))
+c = Val(InputBox("C: "))
+d = Val(InputBox("D: "))
+
+x = Array(a, b, c, d)
+For i = 0 To UBound(x)
+For j = i To UBound(x)
+If x(i) < x(j) Then
+k = x(i)
+x(i) = x(j)
+x(j) = k
+End If
+Next
+Next
+For i = 0 To UBound(x)
+lst.AddItem x(i)
+Next
