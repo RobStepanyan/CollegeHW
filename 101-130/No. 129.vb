f0e3d930bd9470c483de6ed6092fd17c3ea3ce61
Robert <robert1stepanyan@gmail.com>
2019-01-17 16:00:51 +0400

    Create No. 129.vb

--- 101-130/No. 129.vb
+++ 101-130/No. 129.vb
@@ -1,5 +1,4 @@
 '129
-s = 1
 For i = 1 To 16
 If Sqr(i) = Round(Sqr(i)) Then
 x = Tan(i)
@@ -7,6 +6,6 @@ Else
 x = 1 / Tan(i)
 End If
 y = Exp(i - 4)
-s = s * (x * y - i)
+s = s + (x * y - i)
 Next
 lst.AddItem s
