1f3d5262fdb53ed322d785876a77fe25701ef655
Robert <robert1stepanyan@gmail.com>
2019-01-17 14:43:59 +0400

    Create No. 113.vb

--- 101-130/No. 113.vb
+++ 101-130/No. 113.vb
@@ -2,6 +2,6 @@
 n = Val(InputBox("N: "))
 x = Val(InputBox("X: "))
 For i = 0 To n
-s = s + ((1 / (2 * n + 1)) * ((x - 1) / (x ^ 2 + 1)) ^ (2 * n + 1))
+s = s + ((1 / (2 * i + 1)) * ((x - 1) / (x ^ 2 + 1)) ^ (2 * i + 1))
 Next
 lst.AddItem Round(s, 3)
