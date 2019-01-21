e4600bd418542e9a99041d9f89d901d8502d90e0
Robert <robert1stepanyan@gmail.com>
2019-01-21 18:55:31 +0400

    Update No. 169.vb

--- 151-175/No. 169.vb
+++ 151-175/No. 169.vb
@@ -1,10 +1,10 @@
 '169
 x = Val(InputBox("X: "))
 y = Val(InputBox("Y: "))
-z = True
+z = 5
 For i = 2 To Sqr(x + y)
 If (x + y) Mod i = 0 Then
-z = False
+z = 6
 End If
 Next
 lst.AddItem "Z = " & z
