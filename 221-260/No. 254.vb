f2dd77843b04440a790f9847341161fb0b7c59bb
Robert <robert1stepanyan@gmail.com>
2019-01-23 15:38:59 +0400

    Create No. 254.vb

---
+++ 221-260/No. 254.vb
@@ -0,0 +1,16 @@
+'254
+Min = x(1)
+For i = 2 To n
+If x(i) < Min Then
+Mi = i
+Min = x(i)
+End If
+Next
+Max = x(1)
+For i = 2 To n
+If x(i) > Max Then
+Mi = i
+Max = x(i)
+End If
+Next
+lst.AddItem Max * Min
