d24b3e77ecbf8efd6d676fa43066b867b681dd72
Robert <robert1stepanyan@gmail.com>
2019-01-27 17:40:30 +0400

    Create No. 451.vb

---
+++ 441-460/No. 451.vb
@@ -0,0 +1,24 @@
+'No 451
+Dim x() As Integer
+n = Val(InputBox("N: "))
+Pic1.Scale (0, 0)-(n + 1, n + 1)
+ReDim x(1 To n, 1 To n) As Integer
+Pic1.Cls
+For i = 1 To n
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+Pic1.CurrentX = j
+Pic1.CurrentY = i
+Pic1.Print x(i, j)
+
+If i+j = n+1 And i = j Then
+s = s + x(i, j)
+q = q +1
+End If
+Next
+Next
+If q <> 0 Then
+MsgBox s/q
+Else
+MsgBox 0
+End If
