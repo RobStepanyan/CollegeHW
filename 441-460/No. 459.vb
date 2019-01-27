a41faf913dd231f2a4f1319c4287b2759e6ab568
Robert <robert1stepanyan@gmail.com>
2019-01-27 17:51:41 +0400

    Create No. 459.vb

---
+++ 441-460/No. 459.vb
@@ -0,0 +1,20 @@
+'No 459
+Dim x() As Integer
+n = Val(InputBox("N: "))
+Pic1.Scale (0, 0)-(n + 1, n + 1)
+ReDim x(1 To n, 1 To n) As Integer
+Pic1.Cls
+s = 1
+For i = 1 To n
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+Pic1.CurrentX = j
+Pic1.CurrentY = i
+Pic1.Print x(i, j)
+
+If x(i, j) < 0 Then
+s = s * x(i, j)
+End If
+Next
+Next
+MsgBox s
