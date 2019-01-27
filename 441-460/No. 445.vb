23026e52e71327bbbfca1006c23a7777804d92ff
Robert <robert1stepanyan@gmail.com>
2019-01-27 17:28:05 +0400

    Create No. 445.vb

---
+++ 441-460/No. 445.vb
@@ -0,0 +1,20 @@
+'No 445
+Dim x() As Integer
+n = Val(InputBox("N: "))
+k = val(Inputbox("K: "))
+Pic1.Scale (0, 0)-(n + 1, n + 1)
+ReDim x(1 To n, 1 To n) As Integer
+Pic1.Cls
+For i = 1 To n
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+Pic1.CurrentX = j
+Pic1.CurrentY = i
+Pic1.Print x(i, j)
+
+If i > j And abs(x(i,j))> k Then
+q = q +1
+End If
+Next
+Next
+MsgBox q
