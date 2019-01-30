331a875c472272990a0be0104005bf980e71ebf9
Robert <robert1stepanyan@gmail.com>
2019-01-30 11:17:14 +0400

    Create No. 482.vb

---
+++ 461-490/No. 482.vb
@@ -0,0 +1,21 @@
+'482
+'-------------------------------
+Dim x() As Integer
+n = Val(InputBox("N: "))
+ReDim x(1 To n, 1 To n)
+p1.Cls
+p1.Scale (0, 0)-(n + 1, n + 1)
+For i = 1 To n
+bac = False
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+p1.CurrentX = j
+p1.CurrentY = i
+p1.Print x(i, j)
+'-------------------------------
+'----Artagrelu zona--------
+If x(i, j) > 0 Then
+s = s * x(i, j)
+End If
+lst.AddItem s
+'---------------------------
