313496fb3d1ef6df078003828057878e649a4d31
Robert <robert1stepanyan@gmail.com>
2019-01-30 11:29:24 +0400

    Create No. 488.vb

---
+++ 461-490/No. 488.vb
@@ -0,0 +1,28 @@
+'488
+'-------------------------------
+Dim x() As Integer
+n = Val(InputBox("N: "))
+ReDim x(1 To n, 1 To n)
+p1.Cls
+p1.Scale (0, 0)-(n + 1, n + 1)
+For i = 1 To n
+bac = False
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+p1.CurrentX = j
+p1.CurrentY = i
+p1.Print x(i, j)
+Next
+Next
+'-------------------------------
+'----Artagrelu zona--------
+For i = 1 To m
+Max = x(i, i)
+For j = i To m
+If x(i, j) > Max Then
+Max = x(i, j)
+End If
+Next
+Next
+lst.AddItem Max
+'---------------------------
