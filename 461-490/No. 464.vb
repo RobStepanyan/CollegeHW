732ab60a434d9f7f87beddf0e9029cc2923dd216
Robert <robert1stepanyan@gmail.com>
2019-01-30 10:32:34 +0400

    Create No. 464.vb

---
+++ 461-490/No. 464.vb
@@ -0,0 +1,24 @@
+'464
+'-------------------------------
+Dim x() As Integer
+m = Val(InputBox("M: "))
+a = Val(InputBox("A: "))
+b = Val(InputBox("B: "))
+ReDim x(1 To m, 1 To m)
+p1.Scale (0, 0)-(m + 1, m + 1)
+For i = 1 To m
+For j = 1 To m
+x(i, j) = Rnd * 100 - 50
+p1.CurrentX = j
+p1.CurrentY = i
+p1.Print x(i, j)
+'-------------------------------
+'----Artagrelu zona--------
+If j >= i Then
+s = s + x(i, j)
+End If
+Next
+lst.AddItem s
+s = 0
+'---------------------------
+Next
