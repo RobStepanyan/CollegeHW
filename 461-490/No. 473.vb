daf5d2031079c22b12644c17ca08bada4fd466ae
Robert <robert1stepanyan@gmail.com>
2019-01-30 10:58:36 +0400

    Create No. 473.vb

---
+++ 461-490/No. 473.vb
@@ -0,0 +1,30 @@
+'473
+'-------------------------------
+Dim x() As Integer
+n = Val(InputBox("N: "))
+ReDim x(1 To n, 1 To n)
+p1.Cls
+p1.Scale (0, 0)-(n + 1, n + 1)
+For i = 1 To n
+bac = False
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+p1.CurrentX = j
+p1.CurrentY = i
+p1.Print x(i, j)
+Next
+Next
+'-------------------------------
+'----Artagrelu zona--------
+Max = x(1, 1)
+For i = 1 To n
+For j = 1 To n
+If i > j And x(i, j) > Max Then
+Max = i + j
+End If
+End If
+Next
+Next
+lst.AddItem Max
+
+'---------------------------
