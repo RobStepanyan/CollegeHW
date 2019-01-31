0a953850e78722b75af55990c83b28efaa215fdf
Robert <robert1stepanyan@gmail.com>
2019-01-31 12:22:18 +0400

    Create No. 494.vb

---
+++ 491-508/No. 494.vb
@@ -0,0 +1,33 @@
+Dim x() As Integer
+n = Val(InputBox("N "))
+ReDim x(1 To n, 1 To 2 * n)
+p1.Scale (0, 0)-(n * 2 + 1, n + 1)
+p2.Scale (0, 0)-(n * 2 + 1, n + 1)
+p1.Cls
+p2.Cls
+For i = 1 To n
+For j = 1 To 2 * n
+x(i, j) = Rnd * 100 - 50
+p1.CurrentX = j
+p1.CurrentY = i
+p1.Print x(i, j)
+Next
+Next
+'----------------------------
+For i = 1 To n
+For ii = i To n
+If x(i, 1) > x(ii, 1) Then
+k = x(i, 1)
+x(i, 1) = x(ii, 1)
+x(ii, 1) = k
+End If
+Next
+Next
+'-------------------------
+For i = 1 To n
+For j = 1 To 2 * n
+p2.CurrentX = j
+p2.CurrentY = i
+p2.Print x(i, j)
+Next
+Next
