2b68697519186fd655fe7b62e4c31eb249b8c612
Robert <robert1stepanyan@gmail.com>
2019-01-31 12:58:41 +0400

    Create No. 503.vb

---
+++ 491-508/No. 503.vb
@@ -0,0 +1,29 @@
+Dim x() As Integer
+n = Val(InputBox("N "))
+ReDim x(1 To n, 1 To n)
+p1.Scale (0, 0)-(n + 1, n + 1)
+p1.Cls
+For i = 1 To n
+For j = 1 To n
+x(i, j) = Rnd * 100 - 50
+p1.CurrentX = j
+p1.CurrentY = i
+p1.Print x(i, j)
+Next
+Next
+'---------------------------
+For i = 1 To n
+Min = 0
+For j = 1 To n
+If x(i, j) Mod 2 = 0 Then
+Min = x(i, j)
+Exit For
+End If
+Next
+For jj = j To n
+If x(i, jj) < Min And x(i, jj) Mod 2 = 1 Then
+Min = x(i, jj)
+End If
+Next
+lst.AddItem Min
+Next
